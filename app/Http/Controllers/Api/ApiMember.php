1226441b888f752d961fb9033f20384fe26e8ea6
nustracommunity <135233902+nustracommunity@users.noreply.github.com>
2023-08-11 12:13:48 +0700

    Update ApiMember.php

--- app/Http/Controllers/Api/ApiMember.php
+++ app/Http/Controllers/Api/ApiMember.php
@@ -99,6 +99,7 @@ class ApiMember extends Controller
             $user_cabang = user_cabang::where('uuid', $uuid)->first();
             $id_cabang = $user_cabang->cabang_id;
             $db_cabang = cabang::where('uuid', $id_cabang)->value('database');
+            $db_cabang = "transaction_$db_cabang"
             $create = DB::table("$db_cabang")->create(
                 [
                     'uuid' => Str::random(60),
