4c7f422798528c1440b41fbb17279a12b1284dff
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2023-09-08 11:31:33 +0700

    ownerapi

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,7 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'ApiCabang'=> \App\Http\Middleware\ApiCabang::class,
         'ApiMember'=> \App\Http\Middleware\ApiMember::class,
+        'ApiOwner'=> \App\Http\Middleware\ApiOwner::class,
     ];
 
     /**
