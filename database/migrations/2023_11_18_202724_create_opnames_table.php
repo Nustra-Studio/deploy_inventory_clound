a680ba81d645d69c0f5ed546c1a7bde081754833
manggala56 <manggalaputra740@gmail.com>
2023-11-19 05:40:06 +0700

    new fix bug db

--- database/migrations/2023_11_18_202724_create_opnames_table.php
+++ database/migrations/2023_11_18_202724_create_opnames_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
     {
         Schema::create('opnames', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->nullabel();
-            $table->string('barcode')->nullabel();
-            $table->string('stock')->nullabel();
-            $table->string('perubahan')->nullabel();
-            $table->string('id_toko')->nullabel();
-            $table->string('status')->nullabel();
-            $table->string('keterangan')->nullabel();
+            $table->string('uuid')->nullable();
+            $table->string('barcode')->nullable();
+            $table->string('stock')->nullable();
+            $table->string('perubahan')->nullable();
+            $table->string('id_toko')->nullable();
+            $table->string('status')->nullable();
+            $table->string('keterangan')->nullable();
             $table->timestamps();
         });
     }
