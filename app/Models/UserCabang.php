a1b079e369ce651f99eef5b130e4236342f0231e
manggala56 <manggalaputra740@gmail.com>
2023-11-23 00:04:21 +0700

    fix infinty redirect

--- app/Models/UserCabang.php
+++ app/Models/UserCabang.php
@@ -4,16 +4,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
 
 class UserCabang extends Authenticatable
 {
+    use HasApiTokens, HasFactory, Notifiable;
     protected $table = 'user_cabangs';
 
     protected $fillable = [
-        'username', 'password',
+        'username',
     ];
-
+    protected $hidden = [
+        'password',
+    ];
     // Sesuaikan atribut dan metode sesuai kebutuhan
 }
 
