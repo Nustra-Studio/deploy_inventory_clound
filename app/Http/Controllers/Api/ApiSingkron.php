a4fb1d3a7aecd23453748612d9a4c3e7600c41bf
manggala56 <manggalaputra740@gmail.com>
2024-04-18 21:33:15 +0700

    singkron

--- app/Http/Controllers/Api/ApiSingkron.php
+++ app/Http/Controllers/Api/ApiSingkron.php
@@ -173,8 +173,7 @@ class ApiSingkron extends Controller
             'uuid' => $request->uuid,
         ];
         try {
-            Log::error('Server: ' . $data);
-            // DB::table('supliers')->insert($data);
+            DB::table('supliers')->insert($data);
             return response()->json(['status' => 'success', 'message' => 'Data berhasil disimpan secara lokal'], 200);
         } catch (\Exception $e) {
             // Tangani pengecualian jika terjadi kesalahan saat menyimpan ke database lokal
