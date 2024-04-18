28e27c3649614e6a03ed7a98093df5ad9697a2b2
manggala56 <manggalaputra740@gmail.com>
2024-04-18 22:05:43 +0700

    singkron

--- app/Http/Controllers/Api/ApiSingkron.php
+++ app/Http/Controllers/Api/ApiSingkron.php
@@ -163,13 +163,17 @@ class ApiSingkron extends Controller
     private function supplier($request){
         $data = $request->input('data');
         $status = $request->input('status');
-        switch($status){
-            case create :
-                DB::table('supliers')->insert($data);
-            break;
-        }
+        try {
+                switch($status){
+                    case "create" :
+                        DB::table('supliers')->insert($data);
+                    break;
+                }
             return response()->json(['status' => 'success', 'message' => 'Data berhasil disimpan secara lokal'], 200);
-        
+        } catch (\Exception $e) {
+            // Tangani pengecualian jika terjadi kesalahan saat menyimpan ke database lokal
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()], 500);
+        }
     }
     private function barang($request)
     {   
